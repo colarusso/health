1ddc8567e9553a858cf8dbad7536be28e2ac8944
David Colarusso <colarusso@users.noreply.github.com>
2015-02-12 10:32:33 -0500

    More meaningful output
    Changed xml text to include service name and error code.

--- scripts/test_write.php
+++ scripts/test_write.php
@@ -2,20 +2,21 @@
 # Include configuration file. 
 include (dirname(__FILE__) . '/../config.php');
 
-$stmt_top = $dbh->prepare("select id,address from health.services");
+$stmt_top = $dbh->prepare("select id,name,address from health.services");
 $stmt_top->execute(array()); 
   while ($row_top = $stmt_top->fetch()) {
 	$serviceID = $row_top['id']; 
+	$name = $row_top['name'];
     	$url = $row_top['address'];
 	$file = $xml_path.$serviceID.'.xml';
-	checkit($serviceID,$file,$url);
+	checkit($serviceID,$file,$url,$name);
 	$dbh = new PDO("mysql:host=localhost;dbname=health", "root", "faH57!#2bNn@");
 	$stmt_bottom = $dbh->prepare("INSERT INTO health.stat (id,stat) VALUES (?,?)");
 	$stmt_bottom->execute(array($serviceID,$up)); 
   }
 $dbh = null;
 	
-function checkit ($serviceID,$file,$url) {
+function checkit ($serviceID,$file,$url,$name) {
 
 	global $up;
 
@@ -41,16 +42,16 @@ function checkit ($serviceID,$file,$url) {
 		if ($row->stat == $up) {
 			echo "$serviceID: no change\n";
 			#uncomment the call below to write xml on every check, not just on change
-			#writeRSS($file,$url,$up);
+			#writeRSS($file,$url,$up,$name,$httpcode);
 		} else {
 			echo "$serviceID: change\n";
-			writeRSS($file,$url,$up);
+			writeRSS($file,$url,$up,$name,$httpcode);
 		}
 	}
 	$dbh = null;
 }
 
-function writeRSS ($file,$url,$up) {	
+function writeRSS ($file,$url,$up,$name,$httpcode) {	
 	$contents = file_get_contents($file);
 	$fh = fopen($file, "w");
 	if (!$fh) {
@@ -58,12 +59,11 @@ function writeRSS ($file,$url,$up) {
 		print_r (error_get_last());
 	} else {
 		$pattern = '/\s{2}<\/channel>\n/i';
-		$system_name =  preg_replace("/(http(s)?:\/\/([^\.])*\.()|\.([^.])*$)/i", "", $url);
 		$replacement .= "	<item>\n";
 		if ($up == 1) {
-			$replacement .= "		<title>$system_name is up</title>\n";
+			$replacement .= "		<title>$name error(s) cleared.</title>\n";
 		} else {
-			$replacement .= "		<title>$system_name is down</title>\n";
+			$replacement .= "		<title>$name error: $httpcode</title>\n";
 		}
 		$uid = time();
 		$replacement .= "		<link>$url/#$uid</link>\n";
@@ -79,7 +79,7 @@ function writeRSS ($file,$url,$up) {
 			$contents = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 			$contents .= "<rss version=\"2.0\">\n";
 			$contents .= "  <channel>\n";
-			$contents .= "	<title>$system_name status</title>\n";
+			$contents .= "	<title>$name status</title>\n";
 			$contents .= "	<link>$url</link>\n";
 			$contents .= "	<description>Has the site's status Changed?</description>\n";
 			$contents .= "	<language>en</language>\n";
